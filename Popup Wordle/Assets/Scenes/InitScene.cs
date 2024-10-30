5c258c66dbf728e9878f1d41262a1f2e4805c606
Dmidecode <benoit.mira@gmail.com>
2024-10-30 22:27:07 +0100

    Ajout du score et du timer

--- Popup Wordle/Assets/Scenes/InitScene.cs
+++ Popup Wordle/Assets/Scenes/InitScene.cs
@@ -1,12 +1,45 @@
 using System.IO;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 
 public class InitScene : MonoBehaviour
 {
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI TimerText;
+    private int score;
+
+    public float targetTime = 60.0f;
+
     void Awake()
     {
-        string path = "Assets/Scenes/test.txt";
+        string path = "Assets/Scenes/words.txt";
         var lines = File.ReadAllLines(path);
+    }
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore(0);
+    }
+
+    private void Update()
+    {
+        targetTime -= Time.deltaTime;
+        TimerText.text = $"{targetTime:0.##} s";
+        if (targetTime <= 0.0f)
+        {
+            timerEnded();
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        ScoreText.text = "Score: " + score;
+    }
+    void timerEnded()
+    {
+        //do your stuff here.
     }
 }
--- Popup Wordle/Assets/Scenes/SC_MainMenu.cs
+++ Popup Wordle/Assets/Scenes/SC_MainMenu.cs
@@ -16,19 +16,19 @@ public class SC_MainMenu : MonoBehaviour
     public void EasyButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
-        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameLevel");
     }
 
     public void HardButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
-        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameLevel");
     }
 
     public void ShakespearButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
-        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameLevel");
     }
 
     public void CreditsButton()
